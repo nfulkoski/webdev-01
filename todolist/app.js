ec297f66067869362fe1a4efb3f6cc694d64d62e
nfulkoski <>
2020-06-18 22:49:59 +1000

    Commit learning task todo list (functional wip v1.0)

---
+++ todolist/app.js
@@ -0,0 +1,52 @@
+/*
+ * @Author NF
+ * @Task A todo list (project to learn EJS - see list.ejs also)
+ * @Version 1.0.0 (functional, non-final)
+*/
+
+/*
+Important learning
+- es6 created 'let' which has local scope when written inside a function
+- whereas var has global scope when written inside a function
+- e.g. multiple different uses of iterating var i = 0 can cause confusion
+- scoping issues associated with var having global scope when defined inside a function,
+- ... "are among the most common cause of bugs in javascript"
+- in retrospect all variables should be constants so there isnt an example in this file!
+- ... but there is in this project. list.ejs. <3
+*/
+
+//jshint esversion:6
+//jshint -W033
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use('*/css',express.static('public/css'));
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// Updating an array is not breach of const term.
+// push and pop still function.
+const item = [ ]
+
+app.get("/", (req, res) => {
+
+const today = new Date()
+const day = today.toLocaleDateString("en-US", {  weekday: "long", day: "numeric", month: "long"})
+
+res.render('list', { today_name: day, newListItem: item })
+
+})
+
+app.post("/", (req, res) => {
+
+item.push(req.body.input_tsk)
+
+res.redirect("/")
+
+
+})
+
+app.listen(3000, () => { console.log("Listening on port 3000.")})
