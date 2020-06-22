2bbeb551bb6995db90ed933cb03e1a9c679eb44b
nfulkoski <38022206+nfulkoski@users.noreply.github.com>
2020-06-22 15:21:44 +1000

    Update auth-app to utilise salting and hashing of passwords with bcrypt for modern standard of security. v1.0.1 functional. Per learning resource, this is security level 4/6.

--- auth-app/app.js
+++ auth-app/app.js
@@ -2,7 +2,7 @@
  * @Author NF
  * @Task 'Secrets' application - to learn authentication and security
  * @Version 1.0.1
-*/
+ */
 
 //jshint esversion:6
 //jshint -W033
@@ -12,36 +12,40 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = new express()
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-mongoose.connect(process.env.DB_LOC, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_LOC, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const userSchema = new mongoose.Schema({
 
-email: String,
-password: String
+  email: String,
+  password: String
 
 })
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] })
 
 const User = new mongoose.model("User", userSchema)
 
 app.get("/", (req, res) => {
 
-res.render("home")
+  res.render("home")
 
 })
 
 app.get("/login", (req, res) => {
 
-res.render("login")
+  res.render("login")
 
 })
 
@@ -50,21 +54,27 @@ app.post("/login", (req, res) => {
   const username = req.body.username
   const password = req.body.password
 
-  User.findOne({ email: username }, (err, foundUser) => {
+  User.findOne({
+    email: username
+  }, (err, foundUser) => {
 
-    if(err) {
+    if (err) {
 
       console.log(err)
 
     } else {
 
-      if(foundUser) {
+      if (foundUser) {
 
-        if(foundUser.password == password) {
+        bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
 
-          res.render("secrets")
+          if (result == true) {
 
-        }
+            res.render("secrets")
+
+          }
+
+        })
 
       }
 
@@ -79,34 +89,42 @@ app.post("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
 
-res.render("register")
+  res.render("register")
 
 })
 
 app.post("/register", (req, res) => {
 
-const newUser = new User({
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
 
-email: req.body.username,
-password: req.body.password
+    const newUser = new User({
+
+      email: req.body.username,
+      password: hash
+
+    })
+
+    newUser.save((err) => {
+
+      if (err) {
+
+        console.log(err)
+
+      } else {
+
+        res.render("secrets")
+
+      }
+
+    })
+
+  })
+
+
 
 })
 
-newUser.save((err) => {
 
-if(err) {
-
-console.log(err)
-
-} else {
-
-res.render("secrets")
-
-}
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000.")
 })
-
-})
-
-
-app.listen(3000, () => { console.log("Listening on port 3000.") })
