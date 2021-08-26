338f4e55033232fa9f0861b37faea29c55beefc1
Nick Fulkoski <>
2021-08-26 16:37:30 +1000

    Monsters rolodex v1.0.0 complete per Udemy React course.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.cmp.jsx'
 import './components/card-list/card-list.styles.css'
 import './App.css';
+
+import { CardList } from './components/card-list/card-list.cmp.jsx'
+import { SearchBox } from './components/search-box/search-box.cmp.jsx'
 
 class App extends Component {
 
   constructor() {
     super();
+
     this.state = {
 
-      monsters: [  ]
+      monsters: [  ],
+
+      searchField: ''
 
     };
 
@@ -21,18 +26,44 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+
+    this.setState({ searchField: e.target.value });
+
+  };
+
   render() {
+
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
     return(
     <div className='App'>
-    <CardList name='test'>
-    {this.state.monsters.map(monster => (
-        <h1 key={monster.id} className='card-list'> { monster.name } </h1>
-      ))}
-      </CardList>
+      <h1> Monsters rolodex </h1>
+      <SearchBox
+        placeholder = 'Search monsters'
+        handleChange = { this.handleChange }
+      />
+
+      <CardList monsters = { filteredMonsters }> </CardList>
+
     </div>
-  )
+    )
+
   }
 
 }
 
 export default App;
+
+/*
+Learning
+- setState is an asynchronous term i.e. javascript doesnt wait for its completion before executing next code.
+-  - thus pass setState(attr, fin) where fin is a function run when setState finishes.
+
+- destructure via const { a, b } = this.group
+- - equivalent of const a = this.group.a   followed by const b = this.group.b
+
+- functional components dont have access to life cycle methods or states however their code is easier to read and write
+- i.e. export const XyZ = () => () vs class XyZ extends Component { }
+*/
