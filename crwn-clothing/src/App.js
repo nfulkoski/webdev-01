a5d6e3db70a544b575fbc18ff22f6bf1ba3708d0
Nick Fulkoski <>
2021-08-27 16:13:59 +1000

    Commenced Crown Clothing - a major component of the Udemy React course. v0.0.1

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from './Components/home.cmp.jsx'
+
+function App() {
+  return (
+    <div>
+    <Home />
+    </div>
+  );
+}
+
+export default App;
