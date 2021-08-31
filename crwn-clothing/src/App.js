3e4c1854d9ac42369dd3f1dfc832949d98a35040
Nick Fulkoski <38022206+nfulkoski@users.noreply.github.com>
2021-08-31 18:24:01 +1000

    Updated Crown Clothing v0.1.2. Now redirects to home if the user is signed in and or attempts to access signup via url

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  { Route, Switch } from 'react-router-dom';
+import  { Route, Switch, Redirect } from 'react-router-dom';
 import { onSnapshot } from 'firebase/firestore'
 
 import './App.css'
@@ -72,7 +72,7 @@ render() {
         <Switch>
           <Route exact path = '/' component = { Home } />
           <Route path = '/shop' component = { Shop } />
-          <Route path = '/signin' component = { Authpage } />
+          <Route exact path = '/signin' render = {() => this.props.currentUser ? (<Redirect to = '/' />) : (<Authpage />)} />
         </Switch>
 
       </div>
@@ -83,13 +83,19 @@ render() {
 
 }
 
+const mapStateToProps = ({ user }) => ({
+
+  currentUser: user.currentUser
+
+});
+
 const mapDispatchToProps = dispatch => ({
 
   setCurrentUser: user => dispatch(setCurrentUser(user))
 
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
 
 
--- crwn-clothing/src/Redux/user-reducer.js
+++ crwn-clothing/src/Redux/user-reducer.js
@@ -1,4 +1,4 @@
-
+import { UserActionTypes } from './user-types.js'
 
 const INITIAL_STATE = {
 
@@ -10,7 +10,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
 
   switch(action.type) {
 
-    case 'SET_CURRENT_USER':
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
